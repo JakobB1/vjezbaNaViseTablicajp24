b6f0d9edd12c9c47f179b2f8a2951f9da07187ef
Jakob Brkić <83577603+JakobB1@users.noreply.github.com>
2021-05-21 13:40:28 +0100

    Update vjezbaNaViseTablicajp24.sql

--- vjezbaNaViseTablicajp24.sql
+++ vjezbaNaViseTablicajp24.sql
@@ -8,3 +8,12 @@ b.orderNumber=c.orderNumber
 inner join customers d on
 c.customerNumber=d.customerNumber 
 where d.customerName like '%k%';
+
+select a.productName 
+from products a inner join orderdetails b
+on a.productCode=b.productCode
+inner join orders c on
+b.orderNumber=c.orderNumber 
+inner join customers d on
+c.customerNumber=d.customerNumber 
+where d.customerName like '%k%';
